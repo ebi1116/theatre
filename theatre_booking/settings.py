aa37677b1b8fb8c0a8a11e84ad146a09cf8e0b69
Ebi <167673871+ebi1116@users.noreply.github.com>
2026-05-30 17:01:00 +0530

    Update settings.py
    AWS commit

--- theatre_booking/settings.py
+++ theatre_booking/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0syyvt(lo*iam@keb5f2$)i9fi*rm&hod-y2g&f(jgv=*ty3_@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
